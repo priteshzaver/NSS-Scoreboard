3b17a4f3281d8a3c4c5a4f6e07d79cb9857dcfe8
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-18 11:39:01 -0500

    part2 complete

--- src/components/MatchupTitle.vue
+++ src/components/MatchupTitle.vue
@@ -3,7 +3,21 @@ const props = defineProps(['team1', 'team2'])
 </script>
 
 <template>
-  <h1>{{ props.team1.name }} vs. {{ props.team2.name }}</h1>
+  <div>
+    <div>
+      <div :class="{ winning: props.team1.score > props.team2.score }">
+        {{ props.team1.name }}
+      </div>
+      <div>vs.</div>
+      <div :class="{ winning: props.team2.score > props.team1.score }">
+        {{ props.team2.name }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.winning {
+  color: goldenrod;
+}
+</style>
